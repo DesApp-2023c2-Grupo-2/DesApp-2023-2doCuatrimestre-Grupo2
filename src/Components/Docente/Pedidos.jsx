c6ffe8203442e3d144ef3d3e1d926d9a5521e428
GabrieldeRivas <gabriel.derivas@estudiantes.unahur.edu.ar>
2024-05-16 21:29:44 -0300

    Se añade el filtro al docente

--- src/Components/Docente/Pedidos.jsx
+++ src/Components/Docente/Pedidos.jsx
@@ -1,7 +1,7 @@
 import PedidoV1 from "./PedidoV1";
 import { getPedidosPorDni } from "../../Services/getPedidosPorDNIService";
 import Header from '../Header/Header'
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { experimentalStyled as styled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -12,6 +12,7 @@ import { userContext } from "../../Context/LabProvider";
 import BasicModal from "../Laboratorio/utils/BasicModal";
 import { Fab } from "@mui/material";
 import Filtros from "../Laboratorio/Filtros";
+import { correctionDate } from "../Laboratorio/utils/formatDate";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -23,16 +24,43 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function Pedidos() {
 
+  const [listaPedidos, setListaPedidos] = useState([]);
+  const [esAdmin, setEsAdmin] = useState("");
+  const { update,  user, userInfo } = useContext(userContext);
+  const [edicionActiva, setEdicionActiva] = useState(false);
 
-  // controlar si es adminitrador
-  const [esAdmin, setEsAdmin] = useState(true)
-  const {user, userInfo, update} = React.useContext(userContext)
-  
+  /********************************************** */
+  const now = correctionDate(new Date())
+  const [tipo_pedido, setTipoPedido] = React.useState("TODOS");
+  const [fecha_utilizacion, set_fecha_utilizacion] = React.useState("");
+  const [fecha_inicio, set_fecha_inicio] = React.useState("");
+  const [fecha_fin, set_fecha_fin] = React.useState(now);
+  const [edificio, set_edificio] = React.useState("TODOS");
+  const [checked, setChecked] = React.useState("TODOS");
+
+  // *******************************
+  const [open, setOpen] = React.useState("");
+  const [scroll, setScroll] = React.useState("paper");
+  const [alert, setAlert] = useState(false)
 
   const [nuevoPedido, setNuevoPedido] = useState(false);
   const [texto, setEncabezado] = useState("DOCENTE");
 
-  const [listaPedidos, setListaPedidos] = useState([]);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const cargarEstado = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+
+    guardarEstadoPedido(event.target.value);
+  };
+
+  const guardarEstadoPedido = (event) => {
+    setTipoPedido(event);
+  };
 
   useEffect(() => {
     let mounted = true;
@@ -66,7 +94,24 @@ function Pedidos() {
         </Box>
       ) : (<NuevoPedido></NuevoPedido>)}
       {/* opcion pantalla */}
-        
+      <Filtros
+        className="filtros-profesor"
+        cargarEstado={cargarEstado}
+        setTipoPedido={setTipoPedido}
+        fecha_fin={fecha_fin}
+        set_fecha_fin={set_fecha_fin}
+        set_fecha_inicio={set_fecha_inicio}
+        fecha_inicio={fecha_inicio}
+        edificio={edificio}
+        set_edificio={set_edificio}
+        tipo_pedido={tipo_pedido}
+        open={Boolean(open)}
+        setOpen={setOpen}
+        handleClose={handleClose}
+        scroll={scroll}
+        alert={alert}
+        setAlert={setAlert}
+      />
       
       {(listaPedidos.length < 1 && !(nuevoPedido)) ?
         (<Box sx={{ flexGrow: 1, md: 2 }}><NoEncontrados /></Box>)
--- src/Services/getPedidosService.js
+++ src/Services/getPedidosService.js
@@ -32,35 +32,45 @@ export async function getCantidadPedidos() {
       throw error; 
     }
   }
-
-
-
-
-export async function axiosGetPedido( tipo_pedido, fecha_inicio, fecha_fin, edificio, checked)  {
-    var params={}    
+  
+  export async function axiosGetPedido(
+    tipo_pedido,
+    fecha_inicio,
+    fecha_fin,
+    edificio,
+    checked,
+    page
+  ) {
+    var params = {};
     try {
-        if (tipo_pedido && tipo_pedido !== "TODOS") {
-            params.tipo_pedido = tipo_pedido;
+      if (tipo_pedido && tipo_pedido !== "TODOS") {
+        params.tipo_pedido = tipo_pedido;
+      }
+      if (edificio && edificio !== "TODOS") {
+        params.edificio = edificio;
+      }
+      if (fecha_inicio && fecha_fin) {
+        params.fecha_inicio = fecha_inicio;
+        params.fecha_fin = fecha_fin;
+      }
+  
+      params.validsOnly = checked;
+      params.page = page;
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/pedido/`,
+        {
+          params,
+          headers: {
+            Authorization: `Bearer ${token()}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
         }
-        if (edificio && edificio !== "TODOS") {
-            params.edificio = edificio;
-        }
-        if (fecha_inicio && fecha_fin) {
-            params.fecha_inicio = fecha_inicio;
-            params.fecha_fin = fecha_fin;
-        }
-        params.validsOnly = checked
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/pedido/`, {
-            params,
-            headers: {        
-              Authorization: `Bearer ${token()}`,
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-        })
-        return response.data;
+      );
+      return response.data;
     } catch (error) {
-        console.error(error);
-        throw error;
+      console.error(error);
+      throw error;
     }
-};
+  }
+
